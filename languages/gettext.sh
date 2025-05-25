997974380d08180dd90327b45d63e8a3d0969cf4
Brasdrive <86996894+DavidCamejo@users.noreply.github.com>
2025-05-25 17:31:33 -0400

    Update gettext.sh

--- languages/gettext.sh
+++ languages/gettext.sh
@@ -3,8 +3,8 @@
 # To generate a new pmpro-generic-gateway.pot, cd to the main /pmpro-generic-gateway/ directory,
 # then execute `languages/gettext.sh` from the command line.
 # then fix the header info (helps to have the old pmpro-generic-gateway.pot open before running script above)
-# then execute `cp languages/pmpro-generic-gateway.pot languages/pmpropbc.po` to copy the .pot to .po
-# then execute `msgfmt languages/pmpropbc.po --output-file languages/pmpropbc.mo` to generate the .mo
+# then execute `cp languages/pmpro-generic-gateway.pot languages/pmprogpg.po` to copy the .pot to .po
+# then execute `msgfmt languages/pmprogpg.po --output-file languages/pmprogpg.mo` to generate the .mo
 #---------------------------
 echo "Updating pmpro-generic-gateway.pot... "
 xgettext -j -o languages/pmpro-generic-gateway.pot \
